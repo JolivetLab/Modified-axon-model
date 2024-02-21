4b08d3e3d4adfb6e3ea3dc08dee7c6c68dc0a48a
soniabalan <74611877+soniabalan@users.noreply.github.com>
2024-02-21 21:51:39 +0100

    added plotting options (alpha and beta on top of gating var)

--- SoniaScripts/channel_properties.m
+++ SoniaScripts/channel_properties.m
@@ -11,15 +11,17 @@ par.sim.tmax.value = 10;
 
 dt = unitsabs(par.sim.dt.units) * par.sim.dt.value;
 
-figure
+
+%% FIGURES (faster without the simulation above)
+figure(1)
 subplot(211)
 hold on
-plot(TIME_VECTOR,MEMBRANE_POTENTIAL(:,nd));
+plot(TIME_VECTOR,MEMBRANE_POTENTIAL(:,nd), 'k', 'LineWidth',1.5);
 title('voltage')
 xlabel('time (ms)')
 ylabel('voltage')
 hold off
-%%
+
 V = MEMBRANE_POTENTIAL(:,nd);
 
 vrest = simunits(par.elec.pas.vrest.units)*par.elec.pas.vrest.value.ref;
@@ -34,14 +36,10 @@ for i=1:length(par.channels)
             b = rateequation(V(v), par.sim.temp, par.channels(i).gates.temp, par.channels(i).gates.beta.q10(j), par.channels(i).gates.beta.equ{j});
             gates{i}{j}((v), 1) = a/(a+b);
         end
-    end
-end
-
-subplot(212)
-hold on
-for i=1:length(par.channels)
-    for j = 1:par.channels(i).gates.number
-        plot(TIME_VECTOR,gates{i}{j},'DisplayName', par.channels(i).channame)
+        figure(1)
+        subplot(212)
+        hold on
+        plot(TIME_VECTOR,gates{i}{j},'DisplayName', par.channels(i).channame, 'LineWidth', 1.5)
     end
 end
 hold off
@@ -50,19 +48,58 @@ title('gating var')
 xlabel('time (ms)')
 
 %% K+ gates specifically
-V = linspace(-120,50,length(TIME_VECTOR));
+V = linspace(-100,100,length(TIME_VECTOR));
 
 i = 4; 
-j = 1; 
-g4 = nan(length(V),1);
-for v=1:length(V)
-    a = rateequation(V(v), par.sim.temp, par.channels(i).gates.temp, par.channels(i).gates.alpha.q10(j), par.channels(i).gates.alpha.equ{j});
-    b = rateequation(V(v), par.sim.temp, par.channels(i).gates.temp, par.channels(i).gates.beta.q10(j), par.channels(i).gates.beta.equ{j});
-    g4(v) = a/(a+b);
-end
+g4 = nan(length(V),2);
+a = nan(length(V), 1);
+b = nan(length(V), 1);
 
-figure
-plot(V,g4)
-xlabel('voltage')
-ylabel('gating var')
+
+for j=1:par.channels(i).gates.number
+    for v=1:length(V)
+        a(v) = rateequation(V(v), par.sim.temp, par.channels(i).gates.temp, par.channels(i).gates.alpha.q10(j), par.channels(i).gates.alpha.equ{j});
+        b(v) = rateequation(V(v), par.sim.temp, par.channels(i).gates.temp, par.channels(i).gates.beta.q10(j), par.channels(i).gates.beta.equ{j});
+        g4(v,j)= a(v)/(a(v)+b(v));
+    end
+    figure(2)
+    subplot(321)
+    hold on
+    plot(V,a,'LineWidth',1.5, 'DisplayName',par.channels(i).gates.label{j})
+    hold off
+    legend 
+    xlabel('mV')
+    title('alpha')
+    
+    if j<2
+        figure(2)
+        subplot(323)
+        plot(V,b,'LineWidth',1.5)
+        xlabel('mV')
+        title('beta - l')
+
+        a1=a;
+    else
+        figure(2)
+        subplot(325)
+        plot(V,b, 'r','LineWidth',1.5)
+        xlabel('mV')
+        title('beta - r')
+    end
+
+    figure(2)
+    subplot(322)
+    hold on
+    plot(V, g4(:,j),'LineWidth', 1.5, 'DisplayName', par.channels(i).gates.label{j})
+    hold off
+    legend
+    xlabel('mV')
+    title('a/(a+b)')
+end
+%
+figure(2)
+subplot(324)
+plot(V,a1.*a, 'LineWidth', 1.5)
+xlabel('mV')
+title('alpha(l)*alpha(r)')
 
